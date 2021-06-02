0ff37648e8e7f138496b0d362230df5465d2a9b8
PAULSTRICKER\vitorcraveiro <vitorcraveiro@stricker.pt>
2021-06-02 16:14:23 +0100

    Implement Repository for MongoDB.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var dataBase = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = dataBase.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
