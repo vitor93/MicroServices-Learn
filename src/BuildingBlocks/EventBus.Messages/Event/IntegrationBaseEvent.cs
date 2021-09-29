358572c3154709221406dee66cda40059c90c732
PAULSTRICKER\vitorcraveiro <vitorcraveiro@stricker.pt>
2021-09-30 00:56:18 +0100

    Add Comunication models for rabbit mq.

---
+++ src/BuildingBlocks/EventBus.Messages/Event/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Messages.Event
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime creationDate)
+        {
+            Id = id;
+            CreationDate = creationDate;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
