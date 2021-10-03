32e5c4a8dfa2f7096b970e7ec5b04c90b5acc9f6
PAULSTRICKER\vitorcraveiro <vitorcraveiro@stricker.pt>
2021-10-03 12:06:16 +0100

    Add RabbitMq Subscriber To Project that will consume the messages from the queue.

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,15 @@
+﻿using EventBus.Messages.Event;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
