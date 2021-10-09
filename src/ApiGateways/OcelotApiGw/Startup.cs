b0127a0514b0dd9a2d9565071837cde6b1ce01be
PAULSTRICKER\vitorcraveiro <vitorcraveiro@stricker.pt>
2021-10-09 22:31:34 +0100

    Add Ocelot Cache manager.

--- src/ApiGateways/OcelotApiGw/Startup.cs
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 namespace OcelotApiGw
 {
@@ -14,8 +15,9 @@ namespace OcelotApiGw
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddOcelot();
+            //Add Ocelot & Cache Manager Services
+            services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
 
         }
 
