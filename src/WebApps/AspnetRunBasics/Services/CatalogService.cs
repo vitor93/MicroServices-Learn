3e009e444839da2f6577d86f14fa8652bc11f307
PAULSTRICKER\vitorcraveiro <vitorcraveiro@stricker.pt>
2021-10-17 19:29:05 +0100

    Implementation of Catalog Service.

--- src/WebApps/AspnetRunBasics/Services/CatalogService.cs
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -1,4 +1,5 @@
-﻿using AspnetRunBasics.Models;
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -15,24 +16,35 @@ namespace AspnetRunBasics.Services
             _client = client ?? throw new ArgumentNullException(nameof(client));
         }
 
-        public Task<CatalogModel> CreateCatalog(CatalogModel model)
+        public async Task<CatalogModel> CreateCatalog(CatalogModel model)
         {
-            throw new NotImplementedException();
+            var response = await _client.PostAsJson($"/Catalog", model);
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.ReadContentAs<CatalogModel>();
+            }
+            else
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
         }
 
-        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync("/Catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
         }
 
-        public Task<CatalogModel> GetCatalog(string id)
+        public async Task<CatalogModel> GetCatalog(string id)
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync($"/Catalog/{id}");
+            return await response.ReadContentAs<CatalogModel>();
         }
 
-        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
         {
-            throw new NotImplementedException();
+            var response = await _client.GetAsync($"/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
         }
     }
 }
